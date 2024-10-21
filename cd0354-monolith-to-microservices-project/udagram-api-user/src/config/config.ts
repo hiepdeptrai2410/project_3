cda5b3db15bee850837d77d827888eaae1149cb6
CaoNgocHiep <ngochiep24102001@gmail.com>
2024-10-21 12:12:30 +0700

    add project 3

---
+++ cd0354-monolith-to-microservices-project/udagram-api-user/src/config/config.ts
@@ -0,0 +1,18 @@
+console.log(`POSTGRES_USERNAME: ${process.env.POSTGRES_USERNAME}`)
+console.log(`POSTGRES_PASSWORD: ${process.env.POSTGRES_PASSWORD}`)
+console.log(`POSTGRES_HOST: ${process.env.POSTGRES_HOST}`)
+
+export const config = {
+  'username': process.env.POSTGRES_USERNAME,
+  'password': process.env.POSTGRES_PASSWORD,
+  'database': process.env.POSTGRES_DB,
+  'host': process.env.POSTGRES_HOST,
+  'dialect': 'postgres',
+  'aws_region': process.env.AWS_REGION,
+  'aws_profile': process.env.AWS_PROFILE,
+  'aws_media_bucket': process.env.AWS_BUCKET,
+  'url': process.env.URL,
+  'jwt': {
+    'secret': process.env.JWT_SECRET,
+  },
+};
